a33acd11e8600d0293cba5e7bb1361a0a833f3fd
sotelo <ryuk200312@gmail.com>
2019-02-11 13:48:22 -0600

    trabajo en equipo

---
+++ el_carpeton/el_trabajon.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+//trabajo original
+int main(int argc, char *argv[]) {
+	int a,b,c;
+	printf("ingrese a:");
+	scanf("%i",&a);
+	printf("ingrese b:");
+	scanf("%i",&b);
+	c=a+b;
+	printf("tu resultado es %i:",c);
+	return 0;
+}
